60a7c3076acdd297e7329a747124076c17d7fd58
Vitaliy <theyamshikov@yandex.ru>
2022-07-15 16:01:17 +0600

    upd

--- src/Domain/Entities/AssignmentEntity.php
+++ src/Domain/Entities/AssignmentEntity.php
@@ -5,7 +5,7 @@ namespace ZnUser\Rbac\Domain\Entities;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Mapping\ClassMetadata;
 use ZnLib\Components\Status\Enums\StatusEnum;
-use ZnCore\Enum\Constraints\Enum;
+use ZnDomain\Сomponents\EnumRepository\Constraints\Enum;
 use ZnDomain\Validator\Interfaces\ValidationByMetadataInterface;
 use ZnDomain\Entity\Interfaces\UniqueInterface;
 use ZnDomain\Entity\Interfaces\EntityIdInterface;
